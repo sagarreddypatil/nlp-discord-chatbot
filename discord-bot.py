1a159a560d058dd3d62fde45a40b75ec250f818d
Sagar Patil <sagarreddypatil@gmail.com>
2021-03-22 21:37:37 -0400

    bot done(i think)

--- discord-bot.py
+++ discord-bot.py
@@ -1,15 +1,33 @@
 import os
-
 import discord
+from transformers import (
+    ConversationalPipeline,
+    Conversation,
+    BlenderbotForConditionalGeneration,
+    BlenderbotTokenizer,
+)
+
+model = BlenderbotForConditionalGeneration.from_pretrained(
+    "facebook/blenderbot-400M-distill"
+)
+tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
+pipeline = ConversationalPipeline(
+    model=model, tokenizer=tokenizer, min_length_for_response=0, framework="pt"
+)
+generation_kwargs = {"num_beams": 3, "min_length": 0, "temperature": 1.5}
+
+print("Loaded Model")
 
 TOKEN = os.getenv("DISCORD_KEY")
-
 client = discord.Client()
+
+people = []
+conversations = []
 
 
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    print(f"Logged in as {client.user}")
 
 
 @client.event
@@ -17,8 +35,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if message.content.startswith("jane "):
+        utterance = message.content[5:]
+        author = message.author.name
+        current_convo = None
+
+        if author in people:
+            current_convo = conversations[people.index(author)]
+            current_convo.add_user_input(utterance)
+        else:
+            people.append(author)
+            current_convo = Conversation(utterance)
+            conversations.append(current_convo)
+
+        pipeline(current_convo, **generation_kwargs)
+        await message.channel.send(dialogue.generated_responses[-1][1:])
 
 
 client.run(TOKEN)
