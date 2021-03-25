78367c4918468bbc23c66584bc12642723ecbc6a
Sagar Patil <sagarreddypatil@gmail.com>
2021-03-25 17:25:50 -0400

    add automatic conversation truncation

--- discord-bot.py
+++ discord-bot.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import discord
+import string
 from transformers import (
     ConversationalPipeline,
     Conversation,
@@ -20,14 +21,18 @@ generation_kwargs = {"num_beams": 3, "min_length": 0, "temperature": 1.5}
 print("Loaded Model")
 
 conversations = {}
-if os.path.exists("conversations.pkl"):
+if os.path.exists(
+    "conversations.pkl"
+):  # Loading cached conversations to preserve state between restarts
     with open("conversations.pkl", "rb") as file:
         conversations = pickle.load(file)
 
 client = discord.Client()
 
 
-def init_convo(author: str, author_display: str):
+def init_convo(
+    author: str, author_display: str
+):  # helper function to initialize all new conversations
     new_convo = Conversation(f"Hello! I am {author_display}")
     new_convo.mark_processed()
     new_convo.append_response(" Hello! My name is Jane")
@@ -36,7 +41,9 @@ def init_convo(author: str, author_display: str):
     return new_convo
 
 
-def select_or_create_convo(author: str, author_display: str):
+def select_or_create_convo(
+    author: str, author_display: str
+):  # Helper function, selects conversation by author if it exists and creates one if it doesn't.
     current_convo = None
 
     if author in conversations:
@@ -47,7 +54,9 @@ def select_or_create_convo(author: str, author_display: str):
     return current_convo
 
 
-def create_embed(author, title: str, description: str, footer=None):
+def create_embed(
+    author, title: str, description: str, footer=None
+):  # helper function to create Discord embed
     embed = discord.Embed(
         title=title, description=description, footer=footer, color=discord.Color.blue()
     )
@@ -57,12 +66,40 @@ def create_embed(author, title: str, description: str, footer=None):
     return embed
 
 
-def generate_history(author_display, current_convo):
+def generate_history(author_display, current_convo):  # pretty prints the conversation
     output = ""
     for is_user, text in list(current_convo.iter_texts())[2:][-14:]:
         name = author_display if is_user else "Jane"
-        output += "{} >> {} \n".format(name, text)
+        output += "{} >> {}\n".format(name, text)
     return output
+
+
+def truncate_convo_to_token_limit(
+    convo,
+):  # Only call after user response and before pipeline processing
+    truncated_input = tokenizer.decode(
+        tokenizer._build_conversation_input_ids(convo)
+    ).split(
+        "   "
+    )  # The three spaces comes from how the tokenizer processes conversations before model input(see BlenderbotTokenizer._build_conversation_input_ids)
+    if truncated_input[0] in list(convo.iter_texts()):
+        return  # Truncation not required
+    if len(truncated_input) == 1:  # Extremely large user input
+        convo.past_user_inputs = []
+        convo.generated_responses = []
+        convo.new_user_input = truncated_input[0].strip(" " + string.punctuation)
+        return
+    first_valid_idx = 0
+    if truncated_input[1] in convo.past_user_inputs:  # second element is a user input
+        first_valid_idx = convo.past_user_inputs.index(truncated_input[1])
+    elif (
+        truncated_input[1] in convo.generated_responses
+    ):  # second element is a response
+        first_valid_idx = convo.generated_responses.index(truncated_input[1]) + 1
+
+    convo.past_user_inputs = convo.past_user_inputs[first_valid_idx:]
+    convo.generated_responses = convo.generated_responses[first_valid_idx:]
+    return
 
 
 @client.event
@@ -103,7 +140,7 @@ async def on_message(message):
                 message.author,
                 title="Message History",
                 description=output,
-                footer="Jane can only remember the last 128 words in the conversation",
+                footer="Jane can only remember the last 128 syllables in the conversation",
             )
             await message.channel.send(embed=embed)
             return
@@ -147,5 +184,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
 
-with open("conversations.pkl", "wb") as file:
+with open(
+    "conversations.pkl", "wb"
+) as file:  # Cacheing conversations to preserve state between restarts
     pickle.dump(conversations, file, protocol=pickle.HIGHEST_PROTOCOL)
