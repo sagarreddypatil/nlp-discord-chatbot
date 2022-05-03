53ab07798e9a0d18717e848be8b6cf3d458cdaf1
Sagar Patil <sagarreddypatil@gmail.com>
2022-05-02 23:42:23 -0400

    add gpt-j

---
+++ gptj-test.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, GPTJForCausalLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+gpt = GPTJForCausalLM.from_pretrained("hivemind/gpt-j-6B-8bit", low_cpu_mem_usage=True)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+gpt.to(device)
+
+prompt = tokenizer("A cat sat on a mat", return_tensors="pt")
+prompt = {key: value.to(device) for key, value in prompt.items()}
+out = gpt.generate(**prompt, min_length=128, max_length=128, do_sample=True)
+tokenizer.decode(out[0])
