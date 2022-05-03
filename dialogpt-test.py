72b27b74eb1d0c1a159be3e12f1dcfcd79306bae
Sagar Patil <sagarreddypatil@gmail.com>
2022-05-02 23:13:12 -0400

    first

--- dialogpt-test.py
+++ dialogpt-test.py
@@ -11,7 +11,8 @@ tokenizer = GPT2TokenizerFast.from_pretrained("microsoft/DialoGPT-medium")
 model = GPT2LMHeadModel.from_pretrained("microsoft/DialoGPT-medium")
 
 # Let's chat for 5 lines
-for step in range(5):
+first = True
+while True:
     # encode the new user input, add the eos_token and return a tensor in Pytorch
     new_user_input_ids = tokenizer.encode(
         input(">> User:") + tokenizer.eos_token, return_tensors="pt"
@@ -20,7 +21,7 @@ for step in range(5):
     # append the new user input tokens to the chat history
     bot_input_ids = (
         torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
-        if step > 0
+        if first
         else new_user_input_ids
     )
 
@@ -45,3 +46,5 @@ for step in range(5):
             )
         )
     )
+
+    first = False
